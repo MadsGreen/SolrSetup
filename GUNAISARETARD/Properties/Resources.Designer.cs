0020eb0e8315ce2df7fe7b9a01c91473903ffad1
MadsGreen <madsdalhoff@gmail.com>
2020-12-04 11:25:22 +0100

    Small adjustments

--- GUNAISARETARD/Properties/Resources.Designer.cs
+++ GUNAISARETARD/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace GUNAISARETARD.Properties
+namespace SolrSetup.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace GUNAISARETARD.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GUNAISARETARD.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SolrSetup.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
