a88cefff557403b86eefe9bb7a4048ac00f20c16
MadsGreen <58658732+MadsGreen@users.noreply.github.com>
2020-12-14 16:12:36 +0100

    Add files via upload

---
+++ SolrSetup/GUNAISARETARD/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace SolrSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Solr = new Guna.UI2.WinForms.Guna2Button();
+            this.Install = new Guna.UI2.WinForms.Guna2Button();
+            this.Clear = new Guna.UI2.WinForms.Guna2Button();
+            this.Index = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.EXIT = new Guna.UI2.WinForms.Guna2Button();
+            this.Minimize = new Guna.UI2.WinForms.Guna2Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Solr
+            // 
+            this.Solr.BorderRadius = 180;
+            this.Solr.CheckedState.Parent = this.Solr;
+            this.Solr.CustomImages.Parent = this.Solr;
+            this.Solr.CustomizableEdges.BottomLeft = false;
+            this.Solr.CustomizableEdges.BottomRight = false;
+            this.Solr.CustomizableEdges.TopLeft = false;
+            this.Solr.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Solr.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Solr.ForeColor = System.Drawing.Color.White;
+            this.Solr.HoverState.Parent = this.Solr;
+            this.Solr.Image = ((System.Drawing.Image)(resources.GetObject("Solr.Image")));
+            this.Solr.ImageOffset = new System.Drawing.Point(-10, 15);
+            this.Solr.ImageSize = new System.Drawing.Size(80, 65);
+            this.Solr.Location = new System.Drawing.Point(267, 126);
+            this.Solr.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Solr.Name = "Solr";
+            this.Solr.ShadowDecoration.Parent = this.Solr;
+            this.Solr.Size = new System.Drawing.Size(240, 222);
+            this.Solr.TabIndex = 0;
+            this.Solr.Click += new System.EventHandler(this.Solr_Click);
+            this.Solr.MouseHover += new System.EventHandler(this.Solr_MouseHover);
+            // 
+            // Install
+            // 
+            this.Install.BorderRadius = 180;
+            this.Install.CheckedState.Parent = this.Install;
+            this.Install.CustomImages.Parent = this.Install;
+            this.Install.CustomizableEdges.BottomLeft = false;
+            this.Install.CustomizableEdges.BottomRight = false;
+            this.Install.CustomizableEdges.TopRight = false;
+            this.Install.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Install.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Install.ForeColor = System.Drawing.Color.White;
+            this.Install.HoverState.Parent = this.Install;
+            this.Install.Image = ((System.Drawing.Image)(resources.GetObject("Install.Image")));
+            this.Install.ImageOffset = new System.Drawing.Point(10, 15);
+            this.Install.ImageSize = new System.Drawing.Size(65, 65);
+            this.Install.Location = new System.Drawing.Point(21, 126);
+            this.Install.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Install.Name = "Install";
+            this.Install.ShadowDecoration.Parent = this.Install;
+            this.Install.Size = new System.Drawing.Size(240, 222);
+            this.Install.TabIndex = 1;
+            this.Install.Click += new System.EventHandler(this.Install_Click);
+            this.Install.MouseHover += new System.EventHandler(this.Install_MouseHover);
+            // 
+            // Clear
+            // 
+            this.Clear.BorderRadius = 180;
+            this.Clear.CheckedState.Parent = this.Clear;
+            this.Clear.CustomImages.Parent = this.Clear;
+            this.Clear.CustomizableEdges.BottomLeft = false;
+            this.Clear.CustomizableEdges.TopLeft = false;
+            this.Clear.CustomizableEdges.TopRight = false;
+            this.Clear.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Clear.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Clear.ForeColor = System.Drawing.Color.White;
+            this.Clear.HoverState.Parent = this.Clear;
+            this.Clear.Image = ((System.Drawing.Image)(resources.GetObject("Clear.Image")));
+            this.Clear.ImageOffset = new System.Drawing.Point(-10, -15);
+            this.Clear.ImageSize = new System.Drawing.Size(80, 65);
+            this.Clear.Location = new System.Drawing.Point(267, 352);
+            this.Clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Clear.Name = "Clear";
+            this.Clear.ShadowDecoration.Parent = this.Clear;
+            this.Clear.Size = new System.Drawing.Size(240, 222);
+            this.Clear.TabIndex = 2;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            this.Clear.MouseHover += new System.EventHandler(this.Clear_MouseHover);
+            // 
+            // Index
+            // 
+            this.Index.BorderRadius = 180;
+            this.Index.CheckedState.Parent = this.Index;
+            this.Index.CustomImages.Parent = this.Index;
+            this.Index.CustomizableEdges.BottomRight = false;
+            this.Index.CustomizableEdges.TopLeft = false;
+            this.Index.CustomizableEdges.TopRight = false;
+            this.Index.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.Index.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Index.ForeColor = System.Drawing.Color.White;
+            this.Index.HoverState.Parent = this.Index;
+            this.Index.Image = ((System.Drawing.Image)(resources.GetObject("Index.Image")));
+            this.Index.ImageOffset = new System.Drawing.Point(10, -15);
+            this.Index.ImageSize = new System.Drawing.Size(80, 65);
+            this.Index.Location = new System.Drawing.Point(21, 352);
+            this.Index.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Index.Name = "Index";
+            this.Index.ShadowDecoration.Parent = this.Index;
+            this.Index.Size = new System.Drawing.Size(240, 222);
+            this.Index.TabIndex = 3;
+            this.Index.Click += new System.EventHandler(this.Index_Click);
+            this.Index.MouseHover += new System.EventHandler(this.Index_MouseHover);
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CircleButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(27)))), ((int)(((byte)(36)))));
+            this.guna2CircleButton1.BorderThickness = 3;
+            this.guna2CircleButton1.CheckedState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.CustomImages.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(60)))));
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.HoverState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2CircleButton1.Image")));
+            this.guna2CircleButton1.ImageOffset = new System.Drawing.Point(1, 0);
+            this.guna2CircleButton1.ImageSize = new System.Drawing.Size(55, 55);
+            this.guna2CircleButton1.Location = new System.Drawing.Point(211, 299);
+            this.guna2CircleButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.ShadowDecoration.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(107, 98);
+            this.guna2CircleButton1.TabIndex = 5;
+            this.guna2CircleButton1.UseTransparentBackground = true;
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            this.guna2CircleButton1.MouseHover += new System.EventHandler(this.guna2CircleButton1_MouseHover);
+            // 
+            // EXIT
+            // 
+            this.EXIT.CheckedState.Parent = this.EXIT;
+            this.EXIT.CustomImages.Parent = this.EXIT;
+            this.EXIT.FillColor = System.Drawing.Color.Transparent;
+            this.EXIT.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.EXIT.ForeColor = System.Drawing.Color.White;
+            this.EXIT.HoverState.Parent = this.EXIT;
+            this.EXIT.Image = ((System.Drawing.Image)(resources.GetObject("EXIT.Image")));
+            this.EXIT.ImageSize = new System.Drawing.Size(40, 40);
+            this.EXIT.Location = new System.Drawing.Point(1015, 4);
+            this.EXIT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EXIT.Name = "EXIT";
+            this.EXIT.ShadowDecoration.Parent = this.EXIT;
+            this.EXIT.Size = new System.Drawing.Size(77, 65);
+            this.EXIT.TabIndex = 7;
+            this.EXIT.Click += new System.EventHandler(this.EXIT_Click);
+            this.EXIT.MouseHover += new System.EventHandler(this.EXIT_MouseHover);
+            // 
+            // Minimize
+            // 
+            this.Minimize.AnimatedGIF = true;
+            this.Minimize.CheckedState.Parent = this.Minimize;
+            this.Minimize.CustomImages.Parent = this.Minimize;
+            this.Minimize.FillColor = System.Drawing.Color.Transparent;
+            this.Minimize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Minimize.ForeColor = System.Drawing.Color.White;
+            this.Minimize.HoverState.Parent = this.Minimize;
+            this.Minimize.Image = ((System.Drawing.Image)(resources.GetObject("Minimize.Image")));
+            this.Minimize.ImageOffset = new System.Drawing.Point(0, 15);
+            this.Minimize.ImageSize = new System.Drawing.Size(40, 5);
+            this.Minimize.Location = new System.Drawing.Point(952, 4);
+            this.Minimize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.ShadowDecoration.Parent = this.Minimize;
+            this.Minimize.Size = new System.Drawing.Size(67, 54);
+            this.Minimize.TabIndex = 8;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(60)))));
+            this.richTextBox1.Location = new System.Drawing.Point(685, 199);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(344, 365);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(311, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(485, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(784, 148);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(27)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(1093, 596);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Minimize);
+            this.Controls.Add(this.EXIT);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.Controls.Add(this.Index);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Install);
+            this.Controls.Add(this.Solr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button Solr;
+        private Guna.UI2.WinForms.Guna2Button Install;
+        private Guna.UI2.WinForms.Guna2Button Clear;
+        private Guna.UI2.WinForms.Guna2Button Index;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2Button EXIT;
+        private Guna.UI2.WinForms.Guna2Button Minimize;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
